051db41d6739e7fd7ff0865eee7f18ea7312e9f7
Oleg <proverkail@mail.ru>
2022-11-27 08:17:46 +0300

    Zadanie3 переделал

--- Zadanie3/Program.cs
+++ Zadanie3/Program.cs
@@ -1,5 +1,65 @@
 ﻿//Напишите программу, которая задаёт массив из 8 случайных целых чисел и выводит отсортированный по модулю массив.
+int size = 8;
 Random rand = new Random();
+int[] numbers = new int[size];
+
+for (int i = 0; i < size; i++)
+{
+    numbers[i]=rand.Next(-100, 101);
+}
+
+Console.WriteLine("Дан массив");
+Console.Write("[");
+for (int i = 0; i < size; i++)
+{
+    Console.Write($"{numbers[i]}, ");
+}
+Console.WriteLine("]");
+
+int temp;
+for (int i = 0; i < numbers.Length - 1; i++)
+{
+    for (int j = i + 1; j < numbers.Length; j++)
+    {
+        if (Math.Abs(numbers[i]) > Math.Abs(numbers[j]))
+        {
+            temp = numbers[i];
+            numbers[i] = numbers[j];
+            numbers[j] = temp;
+        }
+    }
+}
+Console.WriteLine( );
+Console.WriteLine("Вывод отсортированного массива");
+Console.Write("[");
+for (int i = 0; i < size; i++)
+{
+    Console.Write($"{numbers[i]}, ");
+}
+Console.Write("]");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*Random rand = new Random();
 int n1 = rand.Next(-100, 101);
 int n2 = rand.Next(-100, 101);
 int n3 = rand.Next(-100, 101);
@@ -8,25 +68,25 @@ int n5 = rand.Next(-100, 101);
 int n6 = rand.Next(-100, 101);
 int n7 = rand.Next(-100, 101);
 int n8 = rand.Next(-100, 101);
-int[] nums = {n1, n2,n3, n4, n5, n6, n7, n8};
+int[] numbers = {n1, n2,n3, n4, n5, n6, n7, n8};
 Console.WriteLine($"[{n1}, {n2},{n3}, {n4}, {n5}, {n6}, {n7}, {n8}]");
 int temp;
-for (int i = 0; i < nums.Length - 1; i++)
+for (int i = 0; i < numbers.Length - 1; i++)
 {
-    for (int j = i + 1; j < nums.Length; j++)
+    for (int j = i + 1; j < numbers.Length; j++)
     {
-        if (Math.Abs(nums[i]) > Math.Abs(nums[j]))
+        if (Math.Abs(numbers[i]) > Math.Abs(numbers[j]))
         {
-            temp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = temp;
+            temp = numbers[i];
+            numbers[i] = numbers[j];
+            numbers[j] = temp;
         }
     }
 }
 Console.WriteLine("Вывод отсортированного массива");
 Console.Write("[");
-for (int i = 0; i < nums.Length; i++)
+for (int i = 0; i < numbers.Length; i++)
 {
-    Console.Write($"{nums[i]}, ");
+    Console.Write($"{numbers[i]}, ");
 }
-   Console.Write("]");
+   Console.Write("]");*/
