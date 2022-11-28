717a22dc86811d28590df5c446910423b4b754c9
Oleg <proverkail@mail.ru>
2022-11-28 20:35:28 +0300

    исправил ошибки

--- Zadanie2/Program.cs
+++ Zadanie2/Program.cs
@@ -3,4 +3,13 @@ Console.Write("Введите число: ");
 int A = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите в какую степень вы хотите его возвести: ");
 int B = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"{A}^{B} = " + Math.Pow(A, B));
+int Result = A;
+for (int i = 0; i < B; i++)
+{
+    Console.Write($"{A} * {Result} = ");
+    Result = Result * A;
+    Console.Write($"{Result}");
+    Console.WriteLine();
+}
+Console.WriteLine("Результат");
+Console.WriteLine($"{A}^{B} = {Result}");
