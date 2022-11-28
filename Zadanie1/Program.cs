4e791e6b3b77356276be6267cbf4af6bdd887acd
Oleg <proverkail@mail.ru>
2022-11-28 20:49:11 +0300

    Переделал

--- Zadanie1/Program.cs
+++ Zadanie1/Program.cs
@@ -1,44 +1,12 @@
 ﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 Console.Write("Введите число: ");
-double number = Convert.ToInt32(Console.ReadLine());
-int digitIndex = 0;
-double perv = number.ToString()[digitIndex] - '0';
-digitIndex = 1;
-double vtor = number.ToString()[digitIndex] - '0';
-double tret = 0;
-double xhet = 0;
-double patoe = 0;
-double itog = 0;
-if (number >= 100)
+int number = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+while (number > 0)
 {
-    digitIndex = 2;
-    tret = number.ToString()[digitIndex] - '0';
-    if (number >= 1000)
-    {
-        digitIndex = 3;
-        xhet = number.ToString()[digitIndex] - '0';
-    
-        if (number >= 10000)
-        {
-            digitIndex = 4;
-            patoe = number.ToString()[digitIndex] - '0';
-        }
-    }
+    int f = number % 10;
+    result = result + f;
+    number/=10;
 }
-itog = perv + vtor + tret + xhet + patoe;
-if (patoe != 0)
-{
-    Console.WriteLine($"({perv}+{vtor}+{tret}+{xhet}+{patoe}){itog}");
-}
-else if (xhet != 0)
-{
-        Console.WriteLine($"({perv}+{vtor}+{tret}+{xhet}){itog}");
-}
-else if (tret != 0)
-{
-        Console.WriteLine($"({perv}+{vtor}+{tret}){itog}");
-}
-else
-{
-        Console.WriteLine($"({perv}+{vtor}){itog}");
-}
+Console.WriteLine();
+Console.WriteLine($"Cумма цифр в числе: {result}");
